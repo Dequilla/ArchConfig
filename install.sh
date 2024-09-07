ef3c4f20ab55b92be00f3b4899a90c9856033842
Edwin Wallin <e@edwinwallin.com>
2024-09-07 17:18:56 +0200

    Added some if checks to not add duplicate code when rerunning install script. Fixed other minor misses.

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-cp ./config ~/.config
+cp -r ./config $HOME/.config
 
 sudo pacman -S sway swaylock swayidle swaybg dmenu wmenu polkit
 
 # Open sway after TTY login (when logged in in terminal)
-echo "if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then" >> ~/.profile
-echo "	exec sway" >> ~/.profile
-echo "fi" >> ~/.profile
+start_sway_commands='if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+	exec sway
+fi'
+
+if [[ $(cat ~/.profile | grep -i "$start_sway_commands") ]]; then
+	echo $start_sway_commands >> "$HOME/.profile"
+else
+	echo "Start sway command already in .profile."
+fi
+
+if [ ! -d "$HOME/Images/Backgrounds" ]; then
+	echo "~/Images/Backgrounds not found, creating."
+	mkdir -p "$HOME/Images/Backgrounds"
+fi
+echo "Adding background image to ~/Images/Backgrounds/"
+cp "./images/casper-van-battum-gotland-background.jpg" "$HOME/Images/Backgrounds/casper-van-battum-gotland-background.jpg"
