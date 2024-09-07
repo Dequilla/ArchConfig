62e275fe7357114253c9f67701537b5b2dee1e6f
Edwin Wallin <e@edwinwallin.com>
2024-09-07 19:47:47 +0200

    Fixed grep command to check sway exists or not in .profile

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp -r ./config $HOME/.config
+#######################
+# Sway Window Manager #
+#######################
+
+cp -r "./config" "$HOME/.config"
 
 sudo pacman -S sway swaylock swayidle swaybg dmenu wmenu polkit
 
@@ -13,10 +17,11 @@ start_sway_commands='if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
 	exec sway
 fi'
 
-if [[ $(cat ~/.profile | grep -i "$start_sway_commands") ]]; then
-	echo $start_sway_commands >> "$HOME/.profile"
-else
+if grep -q 'exec sway' "$HOME/.profile"
+then
 	echo "Start sway command already in .profile."
+else
+	echo "$start_sway_commands" >> "$HOME/.profile"
 fi
 
 if [ ! -d "$HOME/Images/Backgrounds" ]; then
@@ -25,3 +30,17 @@ if [ ! -d "$HOME/Images/Backgrounds" ]; then
 fi
 echo "Adding background image to ~/Images/Backgrounds/"
 cp "./images/casper-van-battum-gotland-background.jpg" "$HOME/Images/Backgrounds/casper-van-battum-gotland-background.jpg"
+
+######################
+# SDDM Login Manager #
+######################
+
+sudo pacman -S sddm
+
+sudo systemctl enable sddm.service
+
+if [[ -f "/etc/systemd/system/display-manager.service" ]]; then
+	sudo unlink "/etc/systemd/system/display-manager.service"
+fi
+
+sudo ln -sf /usr/lib/systemd/system/sddm.service /etc/systemd/system/display-manager.service 
