3589635a742ffe2f4179d87b92c32cf052a11030
Edwin Wallin <e@edwinwallin.com>
2024-12-05 22:36:43 +0100

    Changed sddm theme.

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -1,9 +1,13 @@
 # Theme
-active=maldives
+active=sddm-astronaut-theme
 theme_path=/usr/share/sddm/themes/$active
+
+sudo pacman -Sy qt6-svg 
+sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git $theme_path
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
 
 echo "[Theme]
 Current=$active" | sudo tee /etc/sddm.conf
 
 echo "[General]
-background=$HOME/Images/Backgrounds/joris-beugels-alaska.jpg" | sudo tee "$theme_path/theme.conf"
+background=$HOME/Images/Backgrounds/joris-beugels-alaska.jpg" | sudo tee "$theme_path/theme.conf.user"
