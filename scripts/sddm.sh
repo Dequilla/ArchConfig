0375c53cc89b84555e12b7e430b19370c7df49ce
Edwin Wallin <e@edwinwallin.com>
2024-12-06 12:20:45 +0100

    Removed uncessary file.

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -2,6 +2,11 @@
 active=sddm-alaska-theme
 theme_path=/usr/share/sddm/themes/$active
 
+if [ -d "$theme_path" ]; then
+	echo "SDDM theme already installed, deleting to update it..."
+	sudo rm -rf "$theme_path"
+fi
+
 sudo git clone https://github.com/Dequilla/sddm-alaska-theme.git $theme_path
 sudo cp /usr/share/sddm/themes/sddm-alaska-theme/Fonts/* /usr/share/fonts/
 
